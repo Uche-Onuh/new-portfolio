d63aaf3102be5abbf3f9bc4011a81ffa92f34c04
Onuh Uche <onuhblaze@gmail.com>
2023-10-30 10:54:07 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import { useEffect } from "react";
 import Aos from "aos";
-
 import "./App.css";
+
+import { Navbar, Footer } from "./components";
 
 function App() {
   useEffect(() => {
-    Aos.init()
-  }, [])
-  return <h1 className="text-primary">Hello world</h1>;
+    Aos.init();
+  }, []);
+  return (
+    <>
+      <nav>
+        <Navbar />
+      </nav>
+        <main>
+          
+        </main>
+      <footer>
+        <Footer />
+      </footer>
+    </>
+  );
 }
 
 export default App;
---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+import logo from "./logo.png";
+import admin from "./admin.png";
+import bank from "./bank.png";
+import gpt3 from "./gpt3.png";
+import great from "./great.png";
+import luxe from "./luxewebsite.png";
+import mel from "./mel.png";
+import monie from "./moniemove.png";
+import mytube from "./Mytube.png";
+import oge from "./oge.png";
+import osi from "./osi.png";
+
+export { logo, admin, bank, gpt3, great, luxe, mel, monie, mytube, oge, osi };
